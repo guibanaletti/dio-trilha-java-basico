45703359826471b02f05ae01cae2c2c934e305ee
Guilherme Banaletti <guilherme_b95@live.com>
2023-06-01 00:55:07 -0300

    teste inicial

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        long saldo;
+
+        System.out.println("\nIniciando o programa... \n");
+
+        System.out.println("Insira o número da conta: ");
+        numero = sc.nextInt();
+
+        System.out.println("\nInsira o número da agência (Formato xxx-x): ");
+        agencia = sc.next();
+
+        System.out.println("\nInsira o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+
+        nomeCliente+= sc.nextLine();
+
+        System.out.println("\nSaldo do cliente: ");
+        saldo = sc.nextLong();
+
+        System.out.println("\n\nOlá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + ", e seu saldo de R$ " + saldo + " já está disponível para saque");
+
+
+
+        
     }
 }
