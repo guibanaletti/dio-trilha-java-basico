24155ad41d04b633bf55cfa95d89773da91278bf
Guilherme Banaletti <guilherme_b95@live.com>
2023-06-09 23:17:08 -0300

    desafio DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        Scanner leitura = new Scanner(System.in);
 
         int numero;
-        String agencia, nomeCliente;
-        long saldo;
+        String agencia, nome, sobrenome, nomeCliente;
+        double saldo;
 
         System.out.println("\nIniciando o programa... \n");
 
         System.out.println("Insira o número da conta: ");
-        numero = sc.nextInt();
+        numero = leitura.nextInt();
 
         System.out.println("\nInsira o número da agência (Formato xxx-x): ");
-        agencia = sc.next();
+        agencia = leitura.next();
 
-        System.out.println("\nInsira o nome do cliente: ");
-        nomeCliente = sc.nextLine();
 
-        nomeCliente+= sc.nextLine();
-
-        System.out.println("\nSaldo do cliente: ");
-        saldo = sc.nextLong();
+        System.out.println("\nInsira o primeiro nome e último sobrenome do cliente: ");
+        nome = leitura.next();
+        sobrenome = leitura.next();
+        nomeCliente = nome + " " + sobrenome;
+        
+        leitura.nextLine();
+        
+        System.out.println("\nSaldo do cliente (formato 0,00): ");
+        saldo = leitura.nextDouble();
 
         System.out.println("\n\nOlá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + ", e seu saldo de R$ " + saldo + " já está disponível para saque");
-
-
 
         
     }
